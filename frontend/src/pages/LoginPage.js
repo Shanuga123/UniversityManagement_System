318b9a83d9c614fee6f1a198ca4f33072c369cdc
Roshan <romeoroshan@gmail.com>
2024-04-25 12:45:30 +0530

    change login page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -77,7 +77,7 @@ const LoginPage = ({ role }) => {
         const password = "zxc"
 
         if (role === "Admin") {
-            const email = "yogendra@123"
+            const email = "yogendra@12"
             const fields = { email, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
