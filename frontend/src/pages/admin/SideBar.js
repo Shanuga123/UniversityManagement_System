fa649600a06b6fbc48e3ab198ae9b3dbf7511e0e
Roshan <romeoroshan@gmail.com>
2024-04-26 18:24:45 +0530

    change admin side bar

--- frontend/src/pages/admin/SideBar.js
+++ frontend/src/pages/admin/SideBar.js
@@ -21,61 +21,106 @@ const SideBar = () => {
                     <ListItemIcon>
                         <HomeIcon color={location.pathname === ("/" || "/Admin/dashboard") ? 'primary' : 'inherit'} />
                     </ListItemIcon>
-                    <ListItemText primary="Home" />
+                    <ListItemText primary="Home"  
+                        primaryTypographyProps={{
+                        color: location.pathname === ("/" || "/Admin/dashboard") ? 'primary' : 'inherit',
+                        fontWeight: 'medium',
+                        variant: 'body2',
+                    }}/>
                 </ListItemButton>
                 <ListItemButton component={Link} to="/Admin/classes">
                     <ListItemIcon>
                         <ClassOutlinedIcon color={location.pathname.startsWith('/Admin/classes') ? 'primary' : 'inherit'} />
                     </ListItemIcon>
-                    <ListItemText primary="Classes" />
+                    <ListItemText primary="Classes" 
+                        primaryTypographyProps={{
+                        color: location.pathname.startsWith('/Admin/classes') ? 'primary' : 'inherit',
+                        fontWeight: 'medium',
+                        variant: 'body2'
+                    }}/>
                 </ListItemButton>
                 <ListItemButton component={Link} to="/Admin/subjects">
                     <ListItemIcon>
                         <AssignmentIcon color={location.pathname.startsWith("/Admin/subjects") ? 'primary' : 'inherit'} />
                     </ListItemIcon>
-                    <ListItemText primary="Subjects" />
+                    <ListItemText primary="Subjects" 
+                        primaryTypographyProps={{
+                        color: location.pathname.startsWith('/Admin/subjects') ? 'primary' : 'inherit',
+                        fontWeight: 'medium',
+                        variant: 'body2'
+                    }}/>
                 </ListItemButton>
                 <ListItemButton component={Link} to="/Admin/teachers">
                     <ListItemIcon>
                         <SupervisorAccountOutlinedIcon color={location.pathname.startsWith("/Admin/teachers") ? 'primary' : 'inherit'} />
                     </ListItemIcon>
-                    <ListItemText primary="Teachers" />
+                    <ListItemText primary="Teachers" 
+                        primaryTypographyProps={{
+                        color: location.pathname.startsWith('/Admin/teachers') ? 'primary' : 'inherit',
+                        fontWeight: 'medium',
+                        variant: 'body2'
+                        }}/>
                 </ListItemButton>
                 <ListItemButton component={Link} to="/Admin/students">
                     <ListItemIcon>
                         <PersonOutlineIcon color={location.pathname.startsWith("/Admin/students") ? 'primary' : 'inherit'} />
                     </ListItemIcon>
-                    <ListItemText primary="Students" />
+                    <ListItemText primary="Students" 
+                        primaryTypographyProps={{
+                        color: location.pathname.startsWith('/Admin/students') ? 'primary' : 'inherit',
+                        fontWeight: 'medium',
+                        variant: 'body2'
+                    }}/>
                 </ListItemButton>
                 <ListItemButton component={Link} to="/Admin/notices">
                     <ListItemIcon>
                         <AnnouncementOutlinedIcon color={location.pathname.startsWith("/Admin/notices") ? 'primary' : 'inherit'} />
                     </ListItemIcon>
-                    <ListItemText primary="Notices" />
+                    <ListItemText primary="Notices" 
+                        primaryTypographyProps={{
+                        color: location.pathname.startsWith('/Admin/notices') ? 'primary' : 'inherit',
+                        fontWeight: 'medium',
+                        variant: 'body2'
+                    }}/>
                 </ListItemButton>
                 <ListItemButton component={Link} to="/Admin/complains">
                     <ListItemIcon>
                         <ReportIcon color={location.pathname.startsWith("/Admin/complains") ? 'primary' : 'inherit'} />
                     </ListItemIcon>
-                    <ListItemText primary="Complains" />
+                    <ListItemText primary="Complains" 
+                        primaryTypographyProps={{
+                        color: location.pathname.startsWith('/Admin/complains') ? 'primary' : 'inherit',
+                        fontWeight: 'medium',
+                        variant: 'body2'
+                    }}/>
                 </ListItemButton>
             </React.Fragment>
-            <Divider sx={{ my: 1 }} />
+            <Divider sx={{ my: 1 }}  />
             <React.Fragment>
-                <ListSubheader component="div" inset>
+                <ListSubheader component="div" inset >
                     User
                 </ListSubheader>
                 <ListItemButton component={Link} to="/Admin/profile">
                     <ListItemIcon>
                         <AccountCircleOutlinedIcon color={location.pathname.startsWith("/Admin/profile") ? 'primary' : 'inherit'} />
                     </ListItemIcon>
-                    <ListItemText primary="Profile" />
+                    <ListItemText primary="Profile" 
+                        primaryTypographyProps={{
+                        color: location.pathname.startsWith('/Admin/profile') ? 'primary' : 'inherit',
+                        fontWeight: 'medium',
+                        variant: 'body2'
+                    }}/>
                 </ListItemButton>
                 <ListItemButton component={Link} to="/logout">
                     <ListItemIcon>
                         <ExitToAppIcon color={location.pathname.startsWith("/logout") ? 'primary' : 'inherit'} />
                     </ListItemIcon>
-                    <ListItemText primary="Logout" />
+                    <ListItemText primary="Logout" 
+                        primaryTypographyProps={{
+                        color: location.pathname.startsWith('/logout') ? 'primary' : 'inherit',
+                        fontWeight: 'medium',
+                        variant: 'body2'
+                    }}/>
                 </ListItemButton>
             </React.Fragment>
         </>
